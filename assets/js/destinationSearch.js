b721195eb3186c3032af71c15004b60ee3117208
TRASHpanda364 <dixonkimiko@gmail.com>
2024-06-29 11:37:46 -0400

    Itinerary save and deletes work. Can change search destination that was pre-set on homepage

--- assets/js/destinationSearch.js
+++ assets/js/destinationSearch.js
@@ -1,9 +1,10 @@
-const destinationArray = JSON.parse(localStorage.getItem('destination')) || []
+let destinationArray = JSON.parse(localStorage.getItem('destination')) || []
+
 function search(event)
 {
   event.preventDefault()
-
-    const rawSearch = $('#searchBar').val().trim()
+    
+    const rawSearch = $('#destination').val().trim()
     const search = rawSearch.replace(" ", "%20")
     fetch(`https://corsproxy.io/?https://maps.googleapis.com/maps/api/place/textsearch/json?&query=${search}&radius=10000&formatted_phone_number&current_opening_hours&rating&website&key=AIzaSyDLZ7B8ucBo6rIiPh3d7FLGnvwF1vdwj_A`).then(function (response) {
         if (response.status === 404) {
@@ -15,10 +16,18 @@ function search(event)
                 console.log(data)
                 const lat = data.results[0].geometry.location.lat
                 const long = data.results[0].geometry.location.lng
-                destinationArray.push(data.results[0].formatted_address)
-                destinationArray.push(lat)
-                destinationArray.push(long)
+                const tempArray = []
+                tempArray.push(data.results[0].formatted_address)
+                tempArray.push(lat)
+                tempArray.push(long)
+                destinationArray = tempArray
                 localStorage.setItem('destination', JSON.stringify(destinationArray))
+                try
+                {
+                    getSearchResults()
+                }
+                catch
+                {}
             })
 
         }
